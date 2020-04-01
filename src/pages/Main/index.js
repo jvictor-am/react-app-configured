e01d1a5bf9af7afca222d130f0ce0ba29393ed3e
joaovictor <joaovictorengp@gmail.com>
2020-03-31 21:32:01 -0300

    react-app configured()

---
+++ src/pages/Main/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+export default function Main() {
+  return (
+    <Title>
+      React App with following configurations:
+      <small>
+        ESLint, Prettier, EditorConfig, Roteamento React & Styled Components
+      </small>
+    </Title>
+  );
+}
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 24px;
+  /* color: #daa520; */
+  color: #000;
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  small {
+    font-size: 14px;
+    color: #abcbbb;
+    margin-left: 10px;
+  }
+`;
